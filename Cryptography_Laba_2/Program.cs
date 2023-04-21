6332ef44c6a1c82c1c4bb777fee1ad259875d1f6
Никита Забелин <asdfghjkliop44@gmail.com>
2023-04-22 00:18:47 +0300

    Mb ready Round Encryption and Key Expnasion

--- Cryptography_Laba_2/Program.cs
+++ Cryptography_Laba_2/Program.cs
@@ -2,39 +2,93 @@
 
 namespace Cryptography_Laba_2
 {
-    public class RijndaelKeyExpanded : IKeyExpanding
+    public class RijndaelRoundEncryptor : IRoundEncrypting
     {
-        public byte Mod { get; set; }
-        public byte BlockLength { get; set; }
-        byte[] RotateLeft(byte[] vec)
+        public bool LastRound { get; set; }
+        public byte[] SubBytes { get; init; }
+
+        public byte[] MixColumnsPolynomial { get; init; }
+
+        public byte[] GetShiftValues(int blockLength)
         {
-            byte[] res = new byte[vec.Length];
-            for (int i = 0; i < 4; i++)
+            int nb = blockLength / 4;
+            if (nb == 4 || nb == 6)
             {
-                res[i] = res[(i + 1) % vec.Length];
+                return new byte[] { 1, 2, 3 };
+            }
+            else if (nb == 8)
+            {
+                return new byte[] { 1, 3, 4 };
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect block length");
+            }
+        }
+
+        private byte[] MixColumns(byte[] state)
+        {
+
+            byte[] res = new byte[state.Length];
+            byte mod = 0b10001;
+            Polynomial pol = new Polynomial();
+
+            for (int i = 0; i < state.Length / 4; i++)
+            {
+                res[i] = (byte)(pol.MultPolynomials(state[i * 4], MixColumnsPolynomial[3], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 1], MixColumnsPolynomial[0], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 2], MixColumnsPolynomial[1], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 3], MixColumnsPolynomial[2], mod));
+                
+                res[i] = (byte)(pol.MultPolynomials(state[i * 4], MixColumnsPolynomial[2], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 1], MixColumnsPolynomial[3], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 2], MixColumnsPolynomial[0], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 3], MixColumnsPolynomial[1], mod));
+                
+                res[i] = (byte)(pol.MultPolynomials(state[i * 4], MixColumnsPolynomial[1], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 1], MixColumnsPolynomial[2], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 2], MixColumnsPolynomial[3], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 3], MixColumnsPolynomial[0], mod));
+                
+                res[i] = (byte)(pol.MultPolynomials(state[i * 4], MixColumnsPolynomial[0], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 1], MixColumnsPolynomial[1], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 2], MixColumnsPolynomial[2], mod) ^
+                                pol.MultPolynomials(state[i * 4 + 3], MixColumnsPolynomial[3], mod));
             }
 
             return res;
         }
-        
-        public byte[] RCon(int i)
+
+        public byte[] RoundEncrypt(byte[] data, byte[] roundKey)
         {
-            ushort res = 0xcb;
-            for (int ind = 0; ind < i + 1; ind++)
+            byte[] c = GetShiftValues(data.Length);
+
+            byte[] res = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
             {
-                res <<= 1;
-                if (res > 0xff)
+                res[i] = SubBytes[data[i]];
+            }
+
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
                 {
-                    res ^= (ushort)((1 << 8) | Mod);
+                    for (int k = 0; k < c[i]; k++)
+                    {
+                        byte t = res[i * 4];
+                        res[j * 4 + i] = res[(i + 1) * 4];
+                        res[(j + 1) * 4 + i] = res[(i + 2) * 4];
+                        res[(j + 2) * 4 + i] = res[(i + 3) * 4];
+                        res[(j + 3) * 4 + i] = t;
+                    }
                 }
             }
 
-            return new byte[] { (byte)res, 0, 0, 0 };
-        }
-        public byte[][] ExpandKey(byte[] key)
-        {
-            return new byte[0][];
-
+            if (!LastRound)
+            {
+                res = MixColumns(res);
+            }
+            return res;
         }
     }
 
@@ -46,7 +100,6 @@ namespace Cryptography_Laba_2
             byte[] res = new byte[256];
             for (int i = 0; i < 256; i++)
             {
-
                 byte tmp = (byte)i;
                 tmp = (byte)(CycleLeftShift(tmp, 1) ^ CycleLeftShift(tmp, 3) ^
                              CycleLeftShift(tmp, 6) ^ 0x05);
@@ -63,8 +116,8 @@ namespace Cryptography_Laba_2
             for (int i = 0; i < 256; i++)
             {
                 byte tmp = pol.FindReverseElement((byte)i, mod);
-                res[i]= (byte)(tmp ^ CycleLeftShift(tmp, 1) ^ CycleLeftShift(tmp, 2) ^
-                                             CycleLeftShift(tmp, 3) ^ CycleLeftShift(tmp, 4) ^ 0x63);
+                res[i] = (byte)(tmp ^ CycleLeftShift(tmp, 1) ^ CycleLeftShift(tmp, 2) ^
+                                CycleLeftShift(tmp, 3) ^ CycleLeftShift(tmp, 4) ^ 0x63);
             }
 
             return res;
@@ -75,13 +128,23 @@ namespace Cryptography_Laba_2
             return (byte)((d << cnt) | (d >> (8 - cnt)));
         }
 
+        private static byte[][] nrTable = new byte[][]
+        {
+            new byte[] { 10, 12, 14 },
+            new byte[] { 12, 12, 14 },
+            new byte[] { 14, 14, 14 }
+        };
+
+        static byte GetRoundsCount(byte blockLength, byte keyLength)
+        {
+            int nb = blockLength / 4;
+            int nk = keyLength / 4;
+            return nrTable[nb / 2 - 2][nk / 2 - 2];
+        }
+
         static void Main()
         {
-            RijndaelKeyExpanded r = new RijndaelKeyExpanded
-            {
-                Mod = new Polynomial().GetStandardIrreduciblePolynomial()
-            };
-            byte[] res = r.RCon(1);
+            byte[] arr = new Polynomial().GetIrreduciblePolynomials();
             int a = 12;
         }
     }
---
+++ Cryptography_Laba_2/RijndaelKeyExpanded.cs
@@ -0,0 +1,101 @@
+﻿using Cryptography_Laba_1;
+
+namespace Cryptography_Laba_2;
+
+public class RijndaelKeyExpanded : IKeyExpanding
+{
+    public byte Mod { get; init; }
+    public byte BlockLength { get; init; }
+    public byte RoundsCount { get; init; }
+    public byte[] SubBytes { get; init; }
+
+    uint RotateLeft(uint vec)
+    {
+        return (vec << 8) | (vec >> (32 - 8));
+    }
+
+    uint SubByte(uint vec)
+    {
+        uint res = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            res |= (uint)(SubBytes[(byte)(res >> (8 * i) & 0b11111111)] << (8 * i));
+        }
+
+        return res;
+    }
+
+    uint RCon(int i)
+    {
+        ushort res = 0xcb;
+        for (int ind = 0; ind < i + 1; ind++)
+        {
+            res <<= 1;
+            if (res > 0xff)
+            {
+                res ^= (ushort)((1 << 8) | Mod);
+            }
+        }
+
+        return (uint)(res << 24);
+    }
+
+    public byte[][] ExpandKey(byte[] key)
+    {
+        int nk = key.Length / 4;
+        int nb = BlockLength / 4;
+
+        uint[] w = new uint[nb * (RoundsCount + 1)];
+
+        for (int i = 0; i < nk; i++)
+        {
+            int iKey = i * 4;
+            w[i] = (uint)(key[iKey] | (key[iKey + 1] << 8) | (key[iKey + 2] << 16) | (key[iKey + 3] << 24));
+        }
+
+        if (RoundsCount < 6)
+        {
+            for (int j = nk; j < nb * (nk + 1); j += nk)
+            {
+                w[j] = w[j - nk] ^ SubByte(RotateLeft(w[j - 1])) ^ RCon(j / nk);
+
+                for (int i = 1; i < nk && i + j < nb * (RoundsCount + 1); i++)
+                {
+                    w[i + j] = w[i + j - nk] ^ w[i + j - 1];
+                }
+            }
+        }
+        else
+        {
+            for (int j = nk; j < nb * (nk + 1); j += nk)
+            {
+                w[j] = w[j - nk] ^ SubByte(RotateLeft(w[j - 1])) ^ RCon(j / nk);
+
+                for (int i = 1; i < 4; i++)
+                {
+                    w[i + j] = w[i + j - nk] ^ w[i + j - 1];
+                }
+
+                w[j + 4] = w[j + 4 - nk] ^ SubByte(w[j + 3]);
+
+                for (int i = 5; i < nk; i++)
+                {
+                    w[i + j] = w[i + j - nk] ^ w[i + j - 1];
+                }
+            }
+        }
+
+        byte[][] res = new byte[RoundsCount][];
+        for (int i = 0; i < w.Length * 4; i++)
+        {
+            if (i % nb == 0)
+            {
+                res[i / nb] = new byte[nb];
+            }
+
+            res[i / nb][i % nb] = (byte)((w[i / nk] >> (8 * (i % nk))) & 0b11111111);
+        }
+
+        return res;
+    }
+}
